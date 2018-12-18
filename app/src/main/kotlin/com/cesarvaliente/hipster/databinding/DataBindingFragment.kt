9b4e4696b25966cef8fa936cc930185e91286e98
Cesar Valiente Gordo <cesar.valiente@gmail.com>
2018-12-18 08:37:10 +0100

    Using databinding and showing user info

--- app/src/main/kotlin/com/cesarvaliente/hipster/databinding/DataBindingFragment.kt
+++ app/src/main/kotlin/com/cesarvaliente/hipster/databinding/DataBindingFragment.kt
@@ -1,8 +1,21 @@
 package com.cesarvaliente.hipster.databinding
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import com.cesarvaliente.hipster.R
 
 class DataBindingFragment : Fragment() {
 
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val binding: DataBindingLayoutBinding = DataBindingUtil.inflate(
+            layoutInflater, R.layout.data_binding_layout, container, false
+        )
+        binding.user = User(name = "Cesar", surname = "Valiente Gordo", age = 38)
+        return binding.root
+    }
 
 }
